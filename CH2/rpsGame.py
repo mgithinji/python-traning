2e4c2ce4f93c4ef7624e23cb06c483802014e8d6
Morgan Githinji <githinjimorgan@gmail.com>
2022-05-17 10:04:19 -0700

    Update rpsGame.py

--- CH2/rpsGame.py
+++ CH2/rpsGame.py
@@ -16,16 +16,33 @@
 # this is a two player game, but only one person can use the program, since they are competing against the computer (second player/opponent)
 import random
 
-player_move = input("Enter a move rock(r) paper(p) or scissors(s): ")
-computer_move = random.choice(['r', 'p', 's'])
+w = 0
+l = 0
+d = 0
 
-# show what the two players' moves are
-print("Player move: {}, Computer move: {}".format(player_move, computer_move))
+while True:
 
-# compare the moves of the player and computer
-if player_move == computer_move:
-    print("Draw")
-elif (player_move == 's' and computer_move == 'p') or (player_move == 'r' and computer_move == 's') or (player_move == 'p' and computer_move == 'r'):
-    print("You win")
-else:
-    print("You lose")
+    player_move = input("Enter a move rock(r), paper(p) or scissors(s) -- or quit(q): ")
+    computer_move = random.choice(['r', 'p', 's'])
+
+    # quitting the game
+    if player_move == 'q':
+        break
+
+    # show what the two players' moves are
+    print("Player move: {}, Computer move: {}".format(player_move, computer_move))
+
+    # compare the moves of the player and computer
+    if player_move == computer_move:
+        d = d + 1 # incrementing the draw statistic; can also use d += 1
+        print("Draw")
+    elif (player_move == 's' and computer_move == 'p') or (player_move == 'r' and computer_move == 's') or (player_move == 'p' and computer_move == 'r'):
+        w = w + 1
+        print("You win")
+    elif (player_move == 'p' and computer_move == 's') or (player_move == 's' and computer_move == 'r') or (player_move == 'r' and computer_move == 'p'):
+        l = l + 1
+        print("You lose")
+    else:
+        print("Invalid move, select r, p, s, or q.")
+
+print("Wins: {}, Losses: {}, Draws: {}".format(w, l, d))
